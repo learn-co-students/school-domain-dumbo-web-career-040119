882d05fa199a9256ef8f606fab2ab70fa50debbe
Matthew Bartl <mattbartl@gmail.com>
2019-04-06 00:25:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :school, :roster, :grade
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !roster.include?(grade) 
+      roster[grade] = []
+    end
+    roster[grade] << student
+  end
+  
+  def grade(input)
+    @roster[input]
+  end
+
+  def sort
+    @roster.transform_values {|x| x.sort}
+  end
+end
