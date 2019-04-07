9a5f128c5d3daf801d317dffffb06ea272246f12
github username <github email address>
2019-04-07 16:59:56 -0400

    finished

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(school)
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+        if roster.keys.include?(grade)
+            roster[grade] << student
+        else
+            roster[grade] = []
+            roster[grade] << student
+        end
+       #binding.pry
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        new_hash = {}
+        roster.map do |key,value|
+
+            new_hash[key] = value.sort
+
+        end
+        new_hash
+    end
+end
