d55b59f3cf4a751b8f081134e6a9cfa7b732ac5e
Kate F <follett.katie@gmail.com>
2019-04-10 02:27:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class School
   attr_reader :name
   attr_accessor :roster
@@ -6,7 +7,19 @@ class School
     @roster = {}
   end
   
-  def add_student(name, grade)
+  def roster
+    @roster
   end
+  
+  def add_student(name, grade)
+    if !roster.include?(grade) 
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+    
 end
-
